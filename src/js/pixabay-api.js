ec7fad21f9086b4a3b17dbde80ebd7ed0708274c
Kulasha3 <kulasha81@gmail.com>
2025-06-29 17:33:09 +0200

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,16 @@
-export function searchImg(userValue) {
-  const BASE_URL = 'https://pixabay.com/';
-  const END_POINT = '/api/';
-  const KEY = '?key=42127236-8bfdbbfbeed8a2dadaca720e8';
-  const PARAMS = `&q=${userValue}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const url = BASE_URL + END_POINT + KEY + PARAMS;
+import axios from 'axios';
 
-  return fetch(url).then(res => res.json());
+export function getImagesByQuery(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '42127236-8bfdbbfbeed8a2dadaca720e8';
+  
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true
+  };
+
+  return axios.get(BASE_URL, { params }).then(response => response.data);
 }
